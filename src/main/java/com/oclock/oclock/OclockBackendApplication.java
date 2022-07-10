071187a53e886b5049357c62c65d28d0f8c44446
robinjoon <robin980108@naver.com>
2022-07-10 11:42:32 +0900

    Chore: 프로젝트 생성

---
+++ src/main/java/com/oclock/oclock/OclockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oclock.oclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OclockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OclockBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oclock/oclock/OclockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oclock.oclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OclockBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
