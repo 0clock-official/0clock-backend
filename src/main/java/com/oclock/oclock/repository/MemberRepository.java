c083f82ab782f1cae495d576dde7dca7a66ba13c
robinjoon <robin980108@naver.com>
2022-07-11 14:03:59 +0900

    Feat: 회원 관련기능 뼈대 추가

---
+++ src/main/java/com/oclock/oclock/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package com.oclock.oclock.repository;
+
+import com.oclock.oclock.dto.Major;
+import com.oclock.oclock.dto.Member;
+
+import java.util.List;
+
+public interface MemberRepository {
+    void addMemberEmail(String email);
+    void compareMemberEmailCode(String email, String code);
+    void addMemberPassword(Member member);
+    void addMemberPrivacy(Member member);
+    Member selectMemberById(long id);
+    Member selectMemberByEmail(String email);
+    Member selectMemberByEmailAndPassword(String email, String password);
+    List<Member> selectRandomMember(Member member);
+}
---
+++ src/main/java/com/oclock/oclock/service/MemberService.java
@@ -0,0 +1,19 @@
+package com.oclock.oclock.service;
+
+import com.oclock.oclock.dto.Member;
+
+import java.io.File;
+
+public interface MemberService {
+    int getJoinStep(String email); // 가입단계 확인
+    // 각 가입 단계
+    void joinStep1(String email);
+    String joinStep2(String email, String code);
+    void joinStep3(String token, String password);
+    void joinStep4(File image);
+    void joinStep5(Member member);
+    //
+    String[] login(Member member); // 로그인
+    void updateMember(Member member); // 회원정보 수정
+    void resetPassword(String email); // 비밀번호 리셋 요청
+}
