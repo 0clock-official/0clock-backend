72c6b535971bde8cef75a63ce6428dac231196dc
robinjoon <robin980108@naver.com>
2022-11-06 17:14:06 +0900

    Fix: 전역 핸들러가 모든 예외를 핸들링하도록 수정

--- src/main/java/com/oclock/oclock/exception/OClockExceptionHandler.java
+++ src/main/java/com/oclock/oclock/exception/OClockExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.oclock.oclock.exception;
 
 import com.oclock.oclock.dto.response.ErrorMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,11 +9,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
+@Slf4j
 public class OClockExceptionHandler {
-    @ExceptionHandler(OClockException.class)
-    public ResponseEntity<ErrorMessage> handle(OClockException e, HttpServletRequest request){
-        ErrorMessage errorMessage = e.getErrorMessage();
-        errorMessage.setRequestId(request.hashCode());
-        return ResponseEntity.status(errorMessage.getCode()).body(errorMessage);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> handle(Exception e, HttpServletRequest request){
+        if(e instanceof OClockException) {
+            OClockException oClockException = (OClockException)e;
+            ErrorMessage errorMessage = oClockException.getErrorMessage();
+            errorMessage.setRequestId(request.hashCode());
+            return ResponseEntity.status(errorMessage.getCode()).body(errorMessage);
+        }else{
+            log.error(e.toString());
+            ErrorMessage errorMessage = ErrorMessage.builder()
+                    .message("알수 없는 이유로 요청이 정상처리 되지 않았습니다.")
+                    .code(500)
+                    .build();
+            return ResponseEntity.status(errorMessage.getCode()).body(errorMessage);
+        }
     }
 }
