6031b60486c7f0d875846c457f50dd73bc238b29
robinjoon <robin980108@naver.com>
2022-12-29 11:28:02 +0900

    Refactor: 학생증 전송 코드 서비스로 이관

--- src/main/java/com/oclock/oclock/service/MemberService.java
+++ src/main/java/com/oclock/oclock/service/MemberService.java
@@ -3,11 +3,13 @@ package com.oclock.oclock.service;
 import com.oclock.oclock.dto.ChattingRoom;
 import com.oclock.oclock.dto.Member;
 import com.oclock.oclock.dto.MemberDto;
+import com.oclock.oclock.dto.StudentCardDto;
 import com.oclock.oclock.model.Email;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +23,7 @@ public interface MemberService {
     void editMyself(long memberId, Map<String, String> body);
 
     void updateFcm(long memberId,String fcmToken);
-    void updateEmailStudentCard(Map<String, String> body);
+    void updateEmailStudentCard(StudentCardDto studentCardDto,Member member) throws IOException;
 
     Member findById(Long id, RowMapper<Member> rowMapper);
 
@@ -39,4 +41,6 @@ public interface MemberService {
     String createRandomCode() throws NoSuchAlgorithmException;
 
     void mergeToken(long id, String verification);
+
+    boolean checkIdCard(Email email);
 }
